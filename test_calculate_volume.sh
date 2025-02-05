a0f9e463fb6fdae0474296d7611a4fbd9b7809e9
maharshi012 <maharshipurohit1999@gmail.com>
2025-02-05 14:39:57 -0500

    Added some more test cases for edge cases

--- test_calculate_volume.sh
+++ test_calculate_volume.sh
@@ -18,4 +18,21 @@ else
     echo "Test 2 Failed"
 fi
 
+# Test 3: Negative numbers should return an error
+result=$(./calculate_volume.sh -5 4 6)
+if [[ "$result" == *"Error: Please enter valid positive integers."* ]]; then
+    echo "Test 3 Passed ✅"
+else
+    echo "Test 3 Failed ❌"
+fi
+
+# Test 4: large numbers should give error
+result=$(./calculate_volume.sh 15000 140000 450000)
+expected=6000000000000
+if [ "$result" -eq "$expected" ]; then
+    echo "Test 4 Passed ✅"
+else
+    echo "Test 4 Failed ❌"
+fi
+
 
